26d687bd46f5dbf35d4baf880753d9428717ac49
yancz <3209542609@qq.com>
2025-02-16 22:45:46 +0800

    通过CMake重新组织项目结构

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     qRegisterMetaType<Cards>("Cards&");
+    qRegisterMetaType<Cards>("Cards");
     Loading w;
     w.show();
     return a.exec();
