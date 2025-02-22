e0a1374d4d3ef84458d98c30587bc752dc5ee6b5
alter1726 <3209542609@qq.com>
2025-02-22 22:19:35 +0800

    补充销毁方法

--- TcpConnection.c
+++ TcpConnection.c
@@ -4,6 +4,17 @@
 
 int processRead(void *arg)
 {
+    struct TcpConnection *conn = (struct TcpConnection *)arg;
+    // 接收数据
+    int count = bufferSocketRead(conn->readBuf, conn->channel->fd);
+    if (count > 0)
+    {
+        // 接收到了 http 请求, 解析http请求
+    }
+    else
+    {
+        // 断开连接
+    }
     return 0;
 }
 
@@ -17,4 +28,21 @@ struct TcpConnection *tcpConnectionInit(int fd, struct EventLoop *evloop)
     conn->channel = channelInit(fd, ReadEvent, processRead, NULL, NULL);
     eventLoopAddTask(evloop, conn->channel, ADD);
     return conn;
+}
+
+int tcpConnectionDestroy(void *arg)
+{
+    struct TcpConnection *conn = (struct TcpConnection *)arg;
+    if (conn != NULL)
+    {
+        if (conn->readBuf && bufferReadableSize(conn->readBuf) == 0 &&
+            conn->writeBuf && bufferReadableSize(conn->writeBuf) == 0)
+        {
+            destroyChannel(conn->evLoop, conn->channel);
+            bufferDestroy(conn->readBuf);
+            bufferDestroy(conn->writeBuf);
+            free(conn);
+        }
+    }
+    return 0;
 }
